8e275103f89410e844c3e8a05e4d885202dfc7ac
Spencer Thacker <thackers@msoe.edu>
2024-11-20 22:35:14 -0600

    Added a signal handler for SIGSEGV and the signal handler prints a message and allows the program to resume execution

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,25 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Spencer Thacker
  * 
  * Brief summary of modifications:
+ * Added a signal handler for SIGSEGV and the signal handler prints a message and allows the program to resume execution.
  */
 
-
 #include <stdio.h>
+#include <signal.h>
+
+// Signal handler for SIGSEGV
+void handle_segfault(int sig) {
+    printf("SIGSEGV received! Segmentation fault occurred.\n");
+}
 
 int main (int argc, char* argv[]) {
+
+    // Register the signal handler for SIGSEGV
+    signal(SIGSEGV, handle_segfault);
+
     // Declare a null pointer
     int* i = NULL;
 
